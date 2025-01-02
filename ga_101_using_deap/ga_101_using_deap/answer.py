020d8b5093fab0bf598aff804c13aa495057deba
thefifthagreement <mickael.vitry@gmail.com>
2025-01-02 09:52:27 +0100

    use of pickle to store the key

--- ga_101_using_deap/ga_101_using_deap/answer.py
+++ ga_101_using_deap/ga_101_using_deap/answer.py
@@ -1,32 +1,31 @@
 import string
+import pickle
+
 
 ALPHABET = string.ascii_letters + " !'.,"
 
-KEY = "Genetic Algorithms are powerful! It's due to their ability to evolve solutions through a process of Selection, Crossover, and Mutation!"  # noqa: E501
+
+# read the key from a file
+def get_key():
+    with open("key.pkl", "rb") as f:
+        return pickle.load(f)
 
 
 # TODO compare a chromosome with the solution
 # => how many character are in the correct position?
 def get_score(chrom) -> float:
-    global KEY
-
     return 0.0
 
 
 def get_mean_score(population) -> float:
-    global KEY
-
     # compute the average score of the population
     scores = [get_score(chrom) for chrom in population]
     return sum(scores) / len(population)
 
 
 def is_answer(chrom) -> bool:
-    global KEY
-
-    return chrom == KEY
+    return chrom == get_key()
 
 
 def answer_len() -> int:
-    global KEY
-    return len(KEY)
+    return len(get_key())
